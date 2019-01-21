e75f87b7309e72d10a8ed4d5b75477e95c2a9d89
a.Zhuk <a.zhuk@petrovich.ru>
2019-01-21 16:12:52 +0300

    commit 3

--- src/model/model.ts
+++ src/model/model.ts
@@ -49,6 +49,7 @@ module BP3D.Model {
       this.roomLoadedCallbacks.fire();
       console.log('sdf1');
       console.log('sdf2');
+      console.log('sdf3');
     }
 
     private exportSerialized(): string {
